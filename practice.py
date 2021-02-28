20597dc47d7895a72d0b8b1c8e8f38e190d4d385
qusehdgns <qusehdgns123@gmail.com>
2021-02-28 19:35:09 +0900

    gitbash test

---
+++ practice.py
@@ -0,0 +1,198 @@
+# # 숫자 자료형
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+# # 문자열 자료형
+# print('풍선')
+# print("나비")
+# print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+# print('ㅋ'*10)
+
+# # boolean 자료형
+# # 참 / 거짓
+# print(5 > 10)
+# print(5 < 10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not False)
+# print(not (5 > 10))
+
+# # 변수
+# # 애완동물을 소개해주세요~
+# animal = "고양이"
+# name = "해피"
+# age = 4
+# hobby = "낮잠"
+# is_adult = age >= 3
+
+# print("우리집 " + animal + "의 이름은 " + name + "에요")
+# hobby = "공놀이"
+# # print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+# # , 사용시 빈칸 한개씩 포함
+# print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+# print(name + "는 어른일까요? " + str(is_adult))
+
+# 주석
+''' 여러 문장 주석 처리
+하는 방법 입니다 '''
+
+'''
+Quiz) 변수를 이용하여 다음 문장을 출력하시오
+
+변수명 : station
+
+변수값 : "사당", "신도림", "인천공항" 순서대로 입력
+
+출력 문장 : XX 행 열차가 들어오고 있습니다.
+'''
+
+# station = "사당"
+
+# print(station + "행 열차가 들어오고 있습니다.")
+
+# # 연산자
+# print(1+1) # 2
+# print(3-2) # 1
+# print(5*2) # 10
+# print(6/3) # 2
+
+# print(2**3) # 2^3 = 8
+# print(5%3) # 나머지 구하기 2
+# print(10%3) # 1
+# print(5//3) # 몫 구하기 1
+# print(10//3) # 3
+
+# print(10 > 3) # True
+# print(4 >= 7) # False
+# print(10 < 3) # False
+# print(5 <= 5) # True
+
+# print(3 == 3) # True
+# print(4 == 2) # False
+# print(3 + 4 == 7) # True
+
+# print(1 != 3) # True
+# print(not(1 != 3)) # False
+
+# print((3 > 0) and (3 < 5)) # True
+# print((3 > 0) & (3 < 5)) # True
+
+# print((3 > 0) or (3 > 5)) # True
+# print((3 > 0) | (3 > 5)) # True
+
+# print(5 > 4 > 3) # True
+# print(5 > 4 > 7) # False
+
+# # 간단한 수식
+# print(2 + 3 * 4) # 14
+# print((2 + 3) * 4) # 14
+# number = 2 + 3 * 4 # 14
+# print(number)
+# number = number + 2 # 16
+# print(number)
+# number += 2 # 18
+# print(number)
+# number *= 2 # 36
+# print(number)
+# number /= 2 # 18
+# print(number)
+# number -= 2 # 16
+# print(number)
+
+# number %= 5 # 0
+# print(number)
+
+# # 숫자 처리 함수
+# print(abs(-5)) # 절대값 5
+# print(pow(4, 2)) # 제곱 4^2 = 4 * 4 = 16
+# print(max(5, 12)) # 최댓값 12
+# print(min(5, 12)) # 최솟값 5
+# print(round(3.14)) # 반올림 3
+# print(round(4.99)) # 반올림 5
+
+# from math import *
+# print(floor(4.99)) # 내림 4
+# print(ceil(3.14)) # 올림 4
+# print(sqrt(16)) # 제곱근 4
+
+# # 랜덤 함수
+# from random import *
+# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+# print(random() * 10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randrange(1, 46)) # 1 ~ 45 이하의 임의의 값 생성
+
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+# print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
+
+'''
+Quiz) 당신은 최근에 코딩 스터디 모임을 새로 만들었습니다.
+월 4회 스터디를 하는데 3번은 온라은으로 하고 1번은 오프라인으로 하기로 했습니다.
+아래 조건에 맞는 오프라인 모임 날짜를 정해주는 프로그램을 작성하시오.
+
+조건1 : 랜덤으로 날짜를 뽑아야 함
+조건2 : 월별 날짜는 다름을 감안하여 최소 일수인 28 이내로 정함
+조건3 : 매월 1~3일은 스터디 준비를 해야하므로 제외
+
+(출력문 예제)
+오프라인 스터디 모임 날짜는 매월 X 일로 선정되었습니다.
+'''
+# from random import *
+# date = randint(4, 28)
+# print("오프라인 스터디 모임 날짜는 매월 " + str(date) + " 일로 선정되었습니다.")
+
+# # 문자열
+# sentence = '나는 소년입니다'
+# print(sentence)
+# sentence2 = "파이썬은 쉬워요"
+# print(sentence2)
+# sentence3 = """
+# 나는 소년이고,
+# 파이썬은 쉬워요
+# """
+# print(sentence3)
+
+# # 슬라이싱
+# jumin = "970422-1234567"
+
+# print("성별 : " + jumin[7])
+# print("연 : " + jumin[0:2]) # 0 부터 2 직전까지 (0, 1)
+# print("월 : " + jumin[2:4])
+# print("일 : " + jumin[4:6])
+
+# print("생년월일 : " + jumin[:6]) # 처음부터 6 직전까지
+# print("뒤 7자리 : " + jumin[7:]) # 7 부터 끝까지
+# print("뒤 7자리 (뒤에부터) : " + jumin[-7:]) # 맨뒤에서 7번째부터 끝까지
+
+# 문자열 처리 함수
