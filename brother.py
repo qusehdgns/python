eed77143f85d4d48a1d0fbcb95f4b5af15df0fa1
qusehdgns <qusehdgns123@gmail.com>
2021-03-04 20:14:38 +0900

    python practice

--- brother.py
+++ brother.py
@@ -1,9 +1,85 @@
-class A:
+# class A:
 
-    a = 0
-    __b = 3
+#     a = 0
+#     __b = 3
 
-    def __init__(self):
-        print('init')
+#     def __init__(self):
+#         print('init')
 
-print(dir(A))
+# print(dir(A))
+
+# arr = []
+
+# for _ in range(9):
+#     arr.append(int(input()))
+
+# print(max(arr))
+# print(arr.index(max(arr)))
+
+# number = list(map(str, range(10)))
+
+# print(number)
+
+# s = "123456789"
+
+# for i in s:
+#     print(i)
+
+# arr = [int(input()) for _ in range(3)]
+
+# number = list(map(str, range(10)))
+
+# result = str(arr[0] * arr[1] * arr[2])
+
+# for i in number:
+#     print(result.count(i))
+
+# import numpy as np
+
+# input_arr = [int(input()) for _ in range(10)]
+
+# input_arr = np.array(input_arr) % 42
+
+# input_arr = list(set(list(input_arr)))
+
+# print(len(input_arr))
+
+# n = int(input())
+
+# arr = list(map(int, input().split()))
+
+# m = max(arr[:n])
+
+# for i, temp in enumerate(arr[:n]):
+#     arr[i] = temp / m * 100
+
+# print(sum(arr)/n)
+
+# n = int(input())
+
+# arr = [input() for _ in range(n)]
+
+# for temp in arr:
+#     i = 0
+#     score = 0
+#     for j in temp:
+#         if j == 'O':
+#             i += 1
+#             score += i
+#         elif j == 'X':
+#             i = 0
+    
+#     print(score)
+
+c = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(c)]
+
+for temp in arr:
+    avg = sum(temp[1:])/temp[0]
+    count = 0
+    for j in temp[1:]:
+        if j > avg:
+            count += 1
+    
+    print("{:.3f}%".format(count/temp[0] * 100))
--- brother_test.py
+++ brother_test.py
@@ -1,3 +1,60 @@
-a = [i*j for i in range(3) for j in range(9)]
+# hour, minute, time = map(int, input().split())
+# minute -= (time%60)
 
-print(a)
+# if minute < 0:
+#     minute += 60
+#     hour -= 1
+
+# hour = (hour - (time//60)) % 24
+
+# print(f"{hour} {minute}")
+
+# n = int(input())
+# result = 0
+
+# for i in range(1, n+1):
+#     result += i
+
+# print(result)
+
+# n, x = map(int, input().split())
+# arr = list(map(int, input().split()))
+
+# result = ""
+
+# for temp in arr[:n]:
+#     if temp < x:
+#         result += str(temp) + ' '
+
+# print(result)
+
+# num = input()
+# count = 0
+# calc = num
+
+# if len(num) == 1:
+#     num = '0' + num
+
+# while True:
+#     count += 1
+#     temp = int(calc[0]) + int(calc[1])
+#     calc = calc[1] + str(temp)[-1]
+#     if num == calc:
+#         break
+        
+# print(count)
+
+import sys
+num = int(sys.stdin.readline().rstrip())
+count = 0
+calc = num
+
+while True:
+    count += 1
+    temp = (calc//10) + (calc%10)
+    calc = (calc%10) + (temp%10)
+    print(calc)
+    if num == calc:
+        break
+        
+print(count)
