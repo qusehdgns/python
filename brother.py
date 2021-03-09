63c25b98eddf3f3510446b1b06d56efedd5e270c
qusehdgns <qusehdgns123@gmail.com>
2021-03-09 16:12:36 +0900

    baekjoon string

--- brother.py
+++ brother.py
@@ -71,15 +71,161 @@
     
 #     print(score)
 
-c = int(input())
+# c = int(input())
 
-arr = [list(map(int, input().split())) for _ in range(c)]
+# arr = [list(map(int, input().split())) for _ in range(c)]
 
-for temp in arr:
-    avg = sum(temp[1:])/temp[0]
-    count = 0
-    for j in temp[1:]:
-        if j > avg:
-            count += 1
+# for temp in arr:
+#     avg = sum(temp[1:])/temp[0]
+#     count = 0
+#     for j in temp[1:]:
+#         if j > avg:
+#             count += 1
     
-    print("{:.3f}%".format(count/temp[0] * 100))
+#     print("{:.3f}%".format(count/temp[0] * 100))
+
+# def delete_calc(max_num):
+#     request_list = list(range(1, max_num + 1))
+
+#     for temp in list(range(1, max_num + 1)):
+#         calc = temp
+#         for i in str(temp):
+#             calc += int(i)
+        
+#         if calc in request_list:
+#             request_list.remove(calc)
+
+#     return request_list
+
+
+# for i in delete_calc(10000):
+#     print(i)
+
+# def count_calc(max_num):
+#     result = set()
+#     for i in range(1, 10):
+#         for j in range(-i, (10-i)):
+#             input_num = i
+#             while input_num <= max_num:
+#                 result.add(input_num)
+#                 temp = (input_num % 10) + j
+#                 if temp < 0 or temp >= 10:
+#                     break
+#                 else:
+#                     input_num = input_num * 10 + temp
+
+#     return len(result)
+
+# print(count_calc(int(input())))
+
+# print(ord(input()))
+
+# n = int(input())
+# str = input()
+# result = 0
+
+# for i in str[:n]:
+#     result += int(i)
+
+# print(result)
+
+# string = input()
+# result = []
+
+# for i in range(ord('a'), ord('z')+1):
+#     result.append(string.find(chr(i)))
+
+# for temp in result:
+#     print(temp, end=' ')
+
+# n = int(input())
+# result = []
+
+# for _ in range(n):
+#     num, string = input().split()
+#     num = int(num)
+
+#     temp_result = ""
+#     for i in string:
+#         temp_result += i * num
+
+#     result.append(temp_result)
+
+# for temp in result:
+#     print(temp)
+
+# string = input().upper()
+# most = ['', 0]
+
+# for n in range(ord('A'), ord('Z') + 1):
+#     if string.count(chr(n)) > most[1]:
+#         most[0] = chr(n)
+#         most[1] = string.count(chr(n))
+#     elif string.count(chr(n)) == most[1]:
+#         most[0] = '?'
+
+# print(most[0])
+
+# print(len(input().split()))
+
+# first, second = map(reversed, input().split())
+
+# arr = input().split()
+
+# for i in range(len(arr)):
+#     arr[i] = arr[i][::-1]
+
+# print(max(arr))
+
+# string = input()
+# time = 0
+
+# for spell in string:
+#     if ord(spell) in list(range(ord('A'), ord('D'))):
+#         time += 3
+#     elif ord(spell) in list(range(ord('D'), ord('G'))):
+#         time += 4
+#     elif ord(spell) in list(range(ord('G'), ord('J'))):
+#         time += 5
+#     elif ord(spell) in list(range(ord('J'), ord('M'))):
+#         time += 6
+#     elif ord(spell) in list(range(ord('M'), ord('P'))):
+#         time += 7
+#     elif ord(spell) in list(range(ord('P'), ord('T'))):
+#         time += 8
+#     elif ord(spell) in list(range(ord('T'), ord('W'))):
+#         time += 9
+#     elif ord(spell) in list(range(ord('W'), ord('Z') + 1)):
+#         time += 10
+
+# print(time)
+
+# string = input()
+
+# alpa = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+
+# for temp in alpa:
+#     if temp in string:
+#         string = string.replace(temp, " ")
+
+# print(len(string))
+
+n = int(input())
+
+strings = [input() for _ in range(n)]
+result = []
+
+for string in strings:
+    if len(string) > 1:
+        check = True
+        for i in range(len(string)-1):
+            if string[i] != string[i+1] and string[i] in string[i+1:]:
+                check = False
+                break
+        
+        if check:
+            result.append(string)
+    else:
+        result.append(string)
+
+print(len(result))
